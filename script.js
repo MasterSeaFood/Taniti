d206930404888b6583d7cbc3a7334088c5a70517
Kenji Le <kenjil@formetco.com>
2025-06-13 15:39:28 -0400

    fix nav and add home info

---
+++ script.js
@@ -0,0 +1,8 @@
+const home = document.getElementById('home');
+const lodging = document.getElementById('lodging');
+const food = document.getElementById('food');
+const transportation = document.getElementById('transportation');
+const attractions = document.getElementById('attractions');
+
+let pagesArry = [home,lodging,food,transportation,attractions]
+
